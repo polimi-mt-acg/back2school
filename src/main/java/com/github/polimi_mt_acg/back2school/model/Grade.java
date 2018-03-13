dd3e42c24e3faa7176d9cd86a93d38ce3b4b3795
Leonardo Arcari <lippol94@gmail.com>
2018-03-13 17:55:52 +0100

    Basic entity classes: - Classroom - Grade - Lecture - Subject
    Signed-off-by: Leonardo Arcari <lippol94@gmail.com>

---
+++ src/main/java/com/github/polimi_mt_acg/back2school/model/Grade.java
@@ -0,0 +1,69 @@
+package com.github.polimi_mt_acg.back2school.model;
+
+import java.time.LocalDate;
+
+public class Grade {
+    private int id;
+    private int subjectId;
+    private int teacherId;
+    private int studentId;
+    private LocalDate date;
+    private String title;
+    private double grade;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getSubjectId() {
+        return subjectId;
+    }
+
+    public void setSubjectId(int subjectId) {
+        this.subjectId = subjectId;
+    }
+
+    public int getTeacherId() {
+        return teacherId;
+    }
+
+    public void setTeacherId(int teacherId) {
+        this.teacherId = teacherId;
+    }
+
+    public int getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(int studentId) {
+        this.studentId = studentId;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public double getGrade() {
+        return grade;
+    }
+
+    public void setGrade(double grade) {
+        this.grade = grade;
+    }
+}
