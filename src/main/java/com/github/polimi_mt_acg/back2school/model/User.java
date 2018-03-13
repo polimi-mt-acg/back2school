a38af66e7555346ac70c417343c9b2c045b7a054
nyxgear <dev@nyxgear.com>
2018-03-13 17:23:16 +0100

    User model init - GORUP WORK

---
+++ src/main/java/com/github/polimi_mt_acg/back2school/model/User.java
@@ -0,0 +1,70 @@
+package com.github.polimi_mt_acg.back2school.model;
+
+import javax.persistence.*;
+
+public class User {
+
+    private int id;
+    private int roleId;
+    private String name;
+    private String surname;
+    private String email;
+    private String password;
+    private String salt;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(int roleId) {
+        this.roleId = roleId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getSalt() {
+        return salt;
+    }
+
+    public void setSalt(String salt) {
+        this.salt = salt;
+    }
+}
---
+++ src/test/java/com/github/polimi_mt_acg/back2school/model/UserTest.java
@@ -0,0 +1,5 @@
+package com.github.polimi_mt_acg.back2school.model;
+
+public class UserTest {
+
+}
