cb25f730cc9bb632c4e78a1902eccd2ac9be916c
nyxgear <dev@nyxgear.com>
2018-03-19 20:36:33 +0100

    collapsed up attribute type for notification

--- src/main/java/com/github/polimi_mt_acg/back2school/model/Notification.java
+++ src/main/java/com/github/polimi_mt_acg/back2school/model/Notification.java
@@ -4,7 +4,8 @@ package com.github.polimi_mt_acg.back2school.model;
 import javax.persistence.*;
 import java.time.LocalDateTime;
 
-@MappedSuperclass
+@Entity
+@Table(name = "notification")
 public class Notification {
 
     @Id
@@ -17,6 +18,20 @@ public class Notification {
             foreignKey = @ForeignKey(name = "NOTIFICATION_CREATOR_ID_FK"))
     private User creator;
 
+    @ManyToOne
+    @JoinColumn(name = "target_user_id",
+            foreignKey = @ForeignKey(name = "NOTIFICATION_TARGET_USER_ID_FK"))
+    private User targetUser;
+
+    @ManyToOne
+    @JoinColumn(name = "target_class_id",
+            foreignKey = @ForeignKey(name = "NOTIFICATION_TARGET_CLASS_ID_FK"))
+    private Class targetClass;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "type")
+    private Type type = Type.GENERAL;
+
     @Column(name = "date")
     private LocalDateTime datatime;
 
@@ -25,7 +40,6 @@ public class Notification {
 
     @Column(name = "text")
     private String text;
-
 
     public int getId() {
         return id;
@@ -41,6 +55,30 @@ public class Notification {
 
     public void setCreator(User creator) {
         this.creator = creator;
+    }
+
+    public User getTargetUser() {
+        return targetUser;
+    }
+
+    public void setTargetUser(User targetUser) {
+        this.targetUser = targetUser;
+    }
+
+    public Class getTargetClass() {
+        return targetClass;
+    }
+
+    public void setTargetClass(Class targetClass) {
+        this.targetClass = targetClass;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public void setType(Type type) {
+        this.type = type;
     }
 
     public LocalDateTime getDatatime() {
@@ -67,6 +105,9 @@ public class Notification {
         this.text = text;
     }
 
+    public enum Type {
+        GENERAL, CLASS, DIRECT
+    }
 }
 
 
