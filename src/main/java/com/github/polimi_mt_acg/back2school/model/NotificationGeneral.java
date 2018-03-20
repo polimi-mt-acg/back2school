81e6339cf82442474312ce33e6c1c96bc854d445
nyxgear <dev@nyxgear.com>
2018-03-20 14:53:47 +0100

    implemented notifications inheritance by single table strategy

--- src/main/java/com/github/polimi_mt_acg/back2school/model/NotificationGeneral.java
+++ src/main/java/com/github/polimi_mt_acg/back2school/model/NotificationGeneral.java
@@ -3,8 +3,7 @@ package com.github.polimi_mt_acg.back2school.model;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
-@Entity
-@Table(name = "notification_general")
-public class NotificationGeneral extends Notification{
+@Entity(name = "notification_general")
+public class NotificationGeneral extends Notification {
 
 }
