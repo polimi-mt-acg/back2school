2cf21788a8188dadc22b95220317cb3c6dd66410
nyxgear <dev@nyxgear.com>
2018-04-03 18:11:40 +0200

    seeds associations init, grades insertion working

--- src/main/java/com/github/polimi_mt_acg/back2school/model/Grade.java
+++ src/main/java/com/github/polimi_mt_acg/back2school/model/Grade.java
@@ -1,12 +1,22 @@
 package com.github.polimi_mt_acg.back2school.model;
 
+import com.github.polimi_mt_acg.back2school.utils.DatabaseHandler;
+import org.hibernate.Session;
+
 import javax.persistence.*;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 import java.time.LocalDate;
+import java.util.List;
+import java.util.logging.Logger;
 
 @Entity
 @Table(name = "grade")
 public class Grade implements DeserializeToPersistInterface {
 
+    private final static Logger LOGGER =
+            Logger.getLogger(Grade.class.getName());
     @Id
     @GeneratedValue
     @Column(name = "id")
@@ -99,7 +109,62 @@ public class Grade implements DeserializeToPersistInterface {
 
     @Override
     public void prepareToPersist() {
-        // TODO implement this by fetching entities according to the seed
-        // fields: seedSubjectName, seedTeacherEmail, seedStudentEmail
+        associateSubject();
+        associateTeacher();
+        associateStudent();
+    }
+
+    private void associateSubject() {
+        Session session = DatabaseHandler.getInstance().getNewSession();
+        CriteriaBuilder builder = session.getCriteriaBuilder();
+
+        CriteriaQuery<Subject> criteria = builder.createQuery(Subject.class);
+        Root<Subject> root = criteria.from(Subject.class);
+        criteria.select(root);
+        criteria.where(builder.equal(root.get(Subject_.name), this.seedSubjectName));
+
+        List<Subject> entities = session.createQuery(criteria).getResultList();
+
+        if (entities.size() != 0) {
+            this.setSubject(entities.get(0));
+        } else {
+            LOGGER.info("NO subject found with name: " + String.valueOf(this.seedSubjectName));
+        }
+    }
+
+    private void associateTeacher() {
+        Session session = DatabaseHandler.getInstance().getNewSession();
+        CriteriaBuilder builder = session.getCriteriaBuilder();
+
+        CriteriaQuery<User> criteria = builder.createQuery(User.class);
+        Root<User> root = criteria.from(User.class);
+        criteria.select(root);
+        criteria.where(builder.equal(root.get(User_.email), this.seedTeacherEmail));
+
+        List<User> entities = session.createQuery(criteria).getResultList();
+
+        if (entities.size() != 0) {
+            this.setTeacher(entities.get(0));
+        } else {
+            LOGGER.info("NO teacher found with email: " + String.valueOf(this.seedTeacherEmail));
+        }
+    }
+
+    private void associateStudent() {
+        Session session = DatabaseHandler.getInstance().getNewSession();
+        CriteriaBuilder builder = session.getCriteriaBuilder();
+
+        CriteriaQuery<User> criteria = builder.createQuery(User.class);
+        Root<User> root = criteria.from(User.class);
+        criteria.select(root);
+        criteria.where(builder.equal(root.get(User_.email), this.seedStudentEmail));
+
+        List<User> entities = session.createQuery(criteria).getResultList();
+
+        if (entities.size() != 0) {
+            this.setStudent(entities.get(0));
+        } else {
+            LOGGER.info("NO student found with email: " + String.valueOf(this.seedStudentEmail));
+        }
     }
 }
