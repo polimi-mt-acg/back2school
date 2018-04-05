013ea2b230db99ba62f8726e6cf3bcb6b8a38642
GG <giacomo.gregg@gmail.com>
2018-04-05 18:47:49 +0200

    Merge NotificationTest problems branch

--- src/main/java/com/github/polimi_mt_acg/back2school/model/NotificationClassParent.java
+++ src/main/java/com/github/polimi_mt_acg/back2school/model/NotificationClassParent.java
@@ -2,7 +2,7 @@ package com.github.polimi_mt_acg.back2school.model;
 
 import javax.persistence.*;
 
-@Entity (name = "notification_class_parent")
+@Entity(name = "notification_class_parent")
 @DiscriminatorValue(value = "CLASS_PARENT")
 public class NotificationClassParent
         extends Notification implements DeserializeToPersistInterface {
@@ -11,11 +11,13 @@ public class NotificationClassParent
     @OneToOne
     @JoinColumn(name = "target_class_parents_id",
             foreignKey = @ForeignKey(name = "TARGET_CLASS_PARENTS_ID_FK"))
-    private SchoolClass targetcp ;
+    private SchoolClass targetcp ;//= new SchoolClass();
 
     public SchoolClass getTarget() { return targetcp; }
 
-    public void setTarget(SchoolClass target) { this.targetcp = target; }
+    public void setTarget(Class target) {
+        this.target = target;
+    }
 
     @Override
     public void prepareToPersist() {
