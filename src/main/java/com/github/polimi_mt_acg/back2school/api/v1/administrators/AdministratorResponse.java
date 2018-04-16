c3a467bbe015b388a61d4edfad4be7c2138a55f5
Leonardo Arcari <lippol94@gmail.com>
2018-04-16 22:28:05 +0200

    Add the POJO class for /administrators REST API response
    Signed-off-by: Leonardo Arcari <lippol94@gmail.com>

---
+++ src/main/java/com/github/polimi_mt_acg/back2school/api/v1/administrators/AdministratorResponse.java
@@ -0,0 +1,22 @@
+package com.github.polimi_mt_acg.back2school.api.v1.administrators;
+
+import com.github.polimi_mt_acg.back2school.model.User;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import java.util.List;
+
+@XmlRootElement
+public class AdministratorResponse {
+
+    @XmlElement
+    private List<User> administrators;
+
+    public AdministratorResponse() {
+    }
+
+    public AdministratorResponse(List<User> administrators) {
+        this.administrators = administrators;
+    }
+
+}
