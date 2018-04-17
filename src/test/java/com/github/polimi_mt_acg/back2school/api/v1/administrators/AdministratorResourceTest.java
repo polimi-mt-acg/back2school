b91b379d5b01fd97e3c4fa94968366503b3a9217
Leonardo Arcari <lippol94@gmail.com>
2018-04-17 18:10:53 +0200

    Add more tests for /administrators API  - test a call for authorized client  - test unauthorized exception for unauthorized client
    Signed-off-by: Leonardo Arcari <lippol94@gmail.com>

--- src/test/java/com/github/polimi_mt_acg/back2school/api/v1/administrators/AdministratorResourceTest.java
+++ src/test/java/com/github/polimi_mt_acg/back2school/api/v1/administrators/AdministratorResourceTest.java
@@ -1,60 +1,129 @@
 package com.github.polimi_mt_acg.back2school.api.v1.administrators;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.polimi_mt_acg.back2school.api.v1.AuthenticationEndpoint;
 import com.github.polimi_mt_acg.back2school.api.v1.JacksonCustomMapper;
+import com.github.polimi_mt_acg.back2school.model.User;
+import com.github.polimi_mt_acg.back2school.utils.DatabaseHandler;
+import com.github.polimi_mt_acg.back2school.utils.DatabaseSeeder;
+import com.github.polimi_mt_acg.back2school.utils.rest.RestFactory;
 import org.glassfish.grizzly.http.server.HttpServer;
 import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
 import org.glassfish.jersey.jackson.JacksonFeature;
 import org.glassfish.jersey.server.ResourceConfig;
-import org.junit.After;
-import org.junit.Before;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
-import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.NotAuthorizedException;
+import javax.ws.rs.client.Invocation;
 import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
 import java.io.IOException;
 import java.net.URI;
+import java.util.List;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 public class AdministratorResourceTest {
-    private static final String BASE_URI = "http://localhost:8080/v1/";
-    private HttpServer server;
-    private WebTarget target;
+    private static HttpServer server;
 
-    @Before
-    public void setUp() throws Exception {
+    @BeforeClass
+    public static void setUp() throws Exception {
+        // Deploy database scenario
+        DatabaseSeeder.deployScenario("scenarioAdministrators");
+
+        // Run HTTP server
         server = startServer();
-
-        Client client = ClientBuilder.newClient();
-        target = client.target(URI.create(BASE_URI));
     }
 
-    @After
-    public void tearDown() throws Exception {
+    @AfterClass
+    public static void tearDown() throws Exception {
+        // Truncate DB
+        DatabaseHandler.getInstance().truncateDatabase();
+
+        // Close HTTP server
         server.shutdownNow();
     }
 
-    @Test
-    public void getAdministrators() throws IOException {
-        String responseMsg = target.path("administrators").request(MediaType.APPLICATION_JSON).get(String.class);
-
-        System.out.println(responseMsg);
-
-        assertEquals(1, 1);
-    }
-
-    private HttpServer startServer() {
+    private static HttpServer startServer() {
         // Create a resource config that scans for JAX-RS resources and providers
         // in com.github.polimi_mt_acg.back2school.api.v1.administrators.resources package
         final ResourceConfig rc = new ResourceConfig()
+                .register(AuthenticationEndpoint.class)
                 .packages("com.github.polimi_mt_acg.back2school.api.v1.administrators")
                 .register(JacksonCustomMapper.class)
                 .register(JacksonFeature.class);
 
         // create and start a new instance of grizzly http server
         // exposing the Jersey application at BASE_URI
-        return GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), rc);
+        return GrizzlyHttpServerFactory.createHttpServer(URI.create(RestFactory.BASE_URI), rc);
+    }
+
+    @Test
+    public void getAdministrators() throws IOException {
+        // Get Database seeds
+        List<User> admins = (List<User>) DatabaseSeeder
+                .getEntitiesListFromSeed("scenarioAdministrators", "users.json");
+
+        // For each administrator
+        for (User admin : admins) {
+            // Build the Client
+            WebTarget target = RestFactory.buildWebTarget();
+            // Authenticate
+            String token = RestFactory.authenticate(admin.getEmail(), admin.getSeedPassword());
+            assertNotNull(token);
+            assertTrue(!token.isEmpty());
+
+            // Set target to /administrators
+            target = target.path("administrators");
+
+            // Set token and build the GET request
+            Invocation request = target
+                    .request(MediaType.APPLICATION_JSON)
+                    .header(HttpHeaders.AUTHORIZATION, token)
+                    .buildGet();
+
+            // Invoke the request
+            AdministratorResponse response = request.invoke(AdministratorResponse.class);
+
+            // Performs assertions
+            assertNotNull(response);
+            for (User u : response.getAdministrators()) {
+                assertNotNull(u);
+                assertEquals(u.getRole(), User.Role.ADMINISTRATOR);
+                assertNotNull(u.getEmail());
+                assertNotNull(u.getName());
+                assertNotNull(u.getSurname());
+            }
+
+            // Print it
+            System.out.println(new ObjectMapper().writerWithDefaultPrettyPrinter().writeValueAsString(response));
+        }
+    }
+
+    @Test(expected = NotAuthorizedException.class)
+    public void getAdministratorsUnauthorized() throws Exception {
+        // Get Database seeds
+        List<User> admins = (List<User>) DatabaseSeeder
+                .getEntitiesListFromSeed("scenarioAdministrators", "users.json");
+
+        // Get a sample administrator
+        User admin = admins.get(0);
+
+        // Build the Client
+        WebTarget target = RestFactory.buildWebTarget();
+
+        // Set target to /administrators
+        target = target.path("administrators");
+
+        // Build the GET request with no auth
+        Invocation request = target
+                .request(MediaType.APPLICATION_JSON)
+                .buildGet();
+
+        // Invoke the request and expect Unauthorized exception
+        AdministratorResponse response = request.invoke(AdministratorResponse.class);
     }
 }
