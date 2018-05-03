abeb81fc58495887b50218567226ffa3ffb26684
Leonardo Arcari <lippol94@gmail.com>
2018-05-04 00:26:31 +0200

    Add 2 endpoints under /notifications  - /notifications/send-to-teachers  - /notifications/send-to-parents  - JUnit tests for new endpoints  - Database seeds for new endpoints
    Signed-off-by: Leonardo Arcari <lippol94@gmail.com>

--- src/main/java/com/github/polimi_mt_acg/back2school/api/v1/notifications/NotificationsResource.java
+++ src/main/java/com/github/polimi_mt_acg/back2school/api/v1/notifications/NotificationsResource.java
@@ -1,24 +1,88 @@
 package com.github.polimi_mt_acg.back2school.api.v1.notifications;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.github.polimi_mt_acg.back2school.api.v1.administrators.AdministratorSecured;
+import com.github.polimi_mt_acg.back2school.model.AuthenticationSession;
+import com.github.polimi_mt_acg.back2school.model.AuthenticationSession_;
 import com.github.polimi_mt_acg.back2school.model.Notification;
+import com.github.polimi_mt_acg.back2school.model.NotificationGeneralParents;
+import com.github.polimi_mt_acg.back2school.model.NotificationGeneralTeachers;
+import com.github.polimi_mt_acg.back2school.model.User;
 import com.github.polimi_mt_acg.back2school.utils.DatabaseHandler;
 import java.util.List;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import org.hibernate.Session;
 
 @Path("notifications")
 public class NotificationsResource {
   @GET
   @Produces(javax.ws.rs.core.MediaType.APPLICATION_JSON)
   @AdministratorSecured
-  public NotificationsResponse getNotifications( ) throws JsonProcessingException {
+  public NotificationsResponse getNotifications() {
     // Get the list of notifications in the database
     List<Notification> notifications =
         DatabaseHandler.getInstance().getListSelectFrom(Notification.class);
 
     return new NotificationsResponse(notifications);
+  }
+
+  @Path("send-to-teachers")
+  @POST
+  @Consumes(javax.ws.rs.core.MediaType.APPLICATION_JSON)
+  @AdministratorSecured
+  public Response sendNotificationToTeachers(
+      NotificationGeneralTeachers notification, @Context HttpHeaders hh) {
+
+    // Query the notification creator
+    DatabaseHandler dhi = DatabaseHandler.getInstance();
+    Session session = dhi.getNewSession();
+    String token = hh.getHeaderString(HttpHeaders.AUTHORIZATION);
+    session.beginTransaction();
+    User creator = getCreator(token, dhi, session);
+
+    // Fill creator field and persist it
+    notification.setCreator(creator);
+    session.persist(notification);
+    session.getTransaction().commit();
+    session.close();
+
+    return Response.ok(notification, MediaType.APPLICATION_JSON).build();
+  }
+
+  @Path("send-to-parents")
+  @POST
+  @Consumes(javax.ws.rs.core.MediaType.APPLICATION_JSON)
+  @AdministratorSecured
+  public Response sendNotificationToParents(
+      NotificationGeneralParents notification, @Context HttpHeaders hh) {
+
+    // Query the notification creator
+    DatabaseHandler dhi = DatabaseHandler.getInstance();
+    Session session = dhi.getNewSession();
+    String token = hh.getHeaderString(HttpHeaders.AUTHORIZATION);
+    session.beginTransaction();
+    User creator = getCreator(token, dhi, session);
+
+    // Fill creator field and persist it
+    notification.setCreator(creator);
+    session.persist(notification);
+    session.getTransaction().commit();
+    session.close();
+
+    return Response.ok(notification, MediaType.APPLICATION_JSON).build();
+  }
+
+  private User getCreator(String token, DatabaseHandler dhi, Session session) {
+    List<AuthenticationSession> results =
+        dhi.getListSelectFromWhereEqual(
+            AuthenticationSession.class, AuthenticationSession_.token, token, session);
+    return results.get(0).getUser();
   }
 }
