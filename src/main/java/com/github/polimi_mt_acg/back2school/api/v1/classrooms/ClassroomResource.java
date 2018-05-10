4757d8d08777dbf1cdc17349ef1b9b35f02edb7c
GG <giacomo.gregg@gmail.com>
2018-05-10 11:51:28 +0200

    api Classroom: added get and post for endpoint /classrooms

--- src/main/java/com/github/polimi_mt_acg/back2school/api/v1/classrooms/ClassroomResource.java
+++ src/main/java/com/github/polimi_mt_acg/back2school/api/v1/classrooms/ClassroomResource.java
@@ -1,16 +1,65 @@
 package com.github.polimi_mt_acg.back2school.api.v1.classrooms;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.github.polimi_mt_acg.back2school.api.v1.administrators.AdministratorSecured;
+import com.github.polimi_mt_acg.back2school.api.v1.subjects.SubjectResponse;
+import com.github.polimi_mt_acg.back2school.model.Classroom;
+import com.github.polimi_mt_acg.back2school.model.Classroom_;
+import com.github.polimi_mt_acg.back2school.utils.DatabaseHandler;
+import org.hibernate.Session;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.List;
 
 @Path("/classrooms")
 public class ClassroomResource {
 
   @GET
-  @Produces("text/html")
-  public String getHtml() {
+  @Produces(MediaType.APPLICATION_JSON)
+  @AdministratorSecured
+  public ClassroomResponse getClassrooms()throws JsonProcessingException {
+    List<Classroom> classrooms =
+            DatabaseHandler.getInstance()
+                    .getListSelectFrom(Classroom.class);
 
-    return "mytest";
+    return new ClassroomResponse(classrooms);
   }
+
+  @POST
+  @Consumes(javax.ws.rs.core.MediaType.APPLICATION_JSON)
+  @AdministratorSecured
+  public Response postClassrooms(
+          Classroom classroom, @Context HttpHeaders hh) {
+
+    DatabaseHandler dhi = DatabaseHandler.getInstance();
+    Session session = dhi.getNewSession();
+    session.beginTransaction();
+    session.persist(classroom);
+    session.getTransaction().commit();
+    session.close();
+
+    return Response.ok(classroom, MediaType.APPLICATION_JSON).build();
+  }
+  /*
+  @Path("{id}")
+  @GET
+  @Consumes(javax.ws.rs.core.MediaType.APPLICATION_JSON)
+  @AdministratorSecured
+  public Classroom getClassroomID (@PathParam("id") int id){
+    DatabaseHandler dhi = DatabaseHandler.getInstance();
+    Session session = dhi.getNewSession();
+    session.beginTransaction();
+    List<Classroom> classrooms =
+            DatabaseHandler.getInstance()
+                    .getListSelectFromWhereEqual(Classroom.class, Classroom_.id, id, session);
+
+    session.close();
+    return classrooms.get(0);
+  }*/
+
 }
+
---
+++ src/main/java/com/github/polimi_mt_acg/back2school/api/v1/classrooms/ClassroomResponse.java
@@ -0,0 +1,43 @@
+package com.github.polimi_mt_acg.back2school.api.v1.classrooms;
+
+import com.github.polimi_mt_acg.back2school.model.Classroom;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import java.util.List;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ * The response to a request to /administrators REST API. The serialized JSON format has the
+ * following structure: <code>{
+ *   "classrooms" : [ {
+ *     "name" : "Name 1",
+ *     "floor" : "floor 1",
+ *   }, ...]
+ * }</code>
+ */
+@XmlRootElement
+public class ClassroomResponse {
+
+    @XmlElement private List<Classroom> classrooms;
+
+    /** Empty constructor. */
+    public ClassroomResponse() {}
+
+    /**
+     * Construct an ClassroomResponse out of a List of subjects. No copy of {@code classrooms} is
+     * performed.
+     */
+    public ClassroomResponse(List<Classroom> classrooms) {
+        this.classrooms = classrooms;
+    }
+
+    @JsonProperty
+    public List<Classroom> getClassrooms() {
+        return classrooms;
+    }
+
+    @JsonProperty
+    public void setClassrooms(List<Classroom> classrooms) {
+        this.classrooms = classrooms;
+    }
+}
