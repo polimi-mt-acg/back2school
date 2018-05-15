be39f4f475f0d246602af1003df011bbe892dcdd
Leonardo Arcari <lippol94@gmail.com>
2018-05-15 18:02:27 +0200

    Fix a bug in AuthenticationSession.getCurrentUser: we need a method for passing a Session reference. Otherwise code that requires to perform lazy fetching of data on "currentUser" crashes. In fact, if getCurrentUser closes the session we can't fetch data lazily any further.
    Signed-off-by: Leonardo Arcari <lippol94@gmail.com>

--- src/main/java/com/github/polimi_mt_acg/back2school/model/AuthenticationSession.java
+++ src/main/java/com/github/polimi_mt_acg/back2school/model/AuthenticationSession.java
@@ -2,8 +2,6 @@ package com.github.polimi_mt_acg.back2school.model;
 
 import com.github.polimi_mt_acg.back2school.utils.DatabaseHandler;
 import com.github.polimi_mt_acg.back2school.utils.RandomStringGenerator;
-import org.hibernate.Session;
-
 import java.time.Duration;
 import java.time.LocalDateTime;
 import java.util.List;
@@ -21,6 +19,7 @@ import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.core.HttpHeaders;
+import org.hibernate.Session;
 
 @Entity
 @Table(name = "authentication_session")
@@ -126,6 +125,16 @@ public class AuthenticationSession implements DeserializeToPersistInterface {
    * @return
    */
   public static User getCurrentUser(ContainerRequestContext requestContext) {
+    Session session = DatabaseHandler.getInstance().getNewSession();
+    session.beginTransaction();
+
+    User currentUser = getCurrentUser(requestContext, session);
+    session.getTransaction().commit();
+    session.close();
+    return currentUser;
+  }
+
+  public static User getCurrentUser(ContainerRequestContext requestContext, Session session) {
     // Get the Authorization header from the request
     String authorizationHeader = requestContext.getHeaderString(HttpHeaders.AUTHORIZATION);
 
@@ -136,7 +145,6 @@ public class AuthenticationSession implements DeserializeToPersistInterface {
 
     // query to get the last valid AuthenticationSession corresponding to the token
     // ------------------------------------
-    Session session = DatabaseHandler.getInstance().getNewSession();
     CriteriaBuilder builder = session.getCriteriaBuilder();
 
     CriteriaQuery<AuthenticationSession> criteria = builder.createQuery(AuthenticationSession.class);
@@ -169,7 +177,6 @@ public class AuthenticationSession implements DeserializeToPersistInterface {
     authSession.setDatetimeLastInteraction(LocalDateTime.now());
     session.save(authSession);
 
-    session.close();
     return authSession.getUser();
   }
 
