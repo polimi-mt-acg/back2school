17fd5f24cf7b699197628d3bfc95f825bb5acc7e
GG <giacomo.gregg@gmail.com>
2018-05-24 15:49:14 +0200

    Updated ClassroomResponse and SubjectResponse

--- src/main/java/com/github/polimi_mt_acg/back2school/api/v1/classrooms/ClassroomResponse.java
+++ src/main/java/com/github/polimi_mt_acg/back2school/api/v1/classrooms/ClassroomResponse.java
@@ -1,43 +1,17 @@
 package com.github.polimi_mt_acg.back2school.api.v1.classrooms;
 
-import com.github.polimi_mt_acg.back2school.model.Classroom;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import java.net.URI;
 import java.util.List;
-import javax.xml.bind.annotation.XmlElement;
-import javax.xml.bind.annotation.XmlRootElement;
 
-/**
- * The response to a request to /administrators REST API. The serialized JSON format has the
- * following structure: <code>{
- *   "classrooms" : [ {
- *     "name" : "Name 1",
- *     "floor" : "floor 1",
- *   }, ...]
- * }</code>
- */
-@XmlRootElement
 public class ClassroomResponse {
+  private List<URI> classrooms;
 
-  @XmlElement private List<Classroom> classrooms;
-
-  /** Empty constructor. */
-  public ClassroomResponse() {}
-
-  /**
-   * Construct an ClassroomResponse out of a List of subjects. No copy of {@code classrooms} is
-   * performed.
-   */
-  public ClassroomResponse(List<Classroom> classrooms) {
-    this.classrooms = classrooms;
-  }
-
-  @JsonProperty
-  public List<Classroom> getClassrooms() {
+  @JsonProperty public List<URI> getClassrooms() {
     return classrooms;
   }
 
-  @JsonProperty
-  public void setClassrooms(List<Classroom> classrooms) {
+  @JsonProperty public void setClassrooms(List<URI> students) {
     this.classrooms = classrooms;
   }
 }
--- src/main/java/com/github/polimi_mt_acg/back2school/api/v1/subjects/SubjectResponse.java
+++ src/main/java/com/github/polimi_mt_acg/back2school/api/v1/subjects/SubjectResponse.java
@@ -1,44 +1,17 @@
 package com.github.polimi_mt_acg.back2school.api.v1.subjects;
 
-import com.github.polimi_mt_acg.back2school.model.Notification;
-import com.github.polimi_mt_acg.back2school.model.Subject;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import java.net.URI;
 import java.util.List;
-import javax.xml.bind.annotation.XmlElement;
-import javax.xml.bind.annotation.XmlRootElement;
 
-/**
- * The response to a request to /administrators REST API. The serialized JSON format has the
- * following structure: <code>{
- *   "subjects" : [ {
- *     "name" : "Name 1",
- *     "description" : "Description 1",
- *   }, ...]
- * }</code>
- */
-@XmlRootElement
 public class SubjectResponse {
+  private List<URI> subjects;
 
-  @XmlElement private List<Subject> subjects;
-
-  /** Empty constructor. */
-  public SubjectResponse() {}
-
-  /**
-   * Construct an SubjectResponse out of a List of subjects. No copy of {@code subjects} is
-   * performed.
-   */
-  public SubjectResponse(List<Subject> subjects) {
-    this.subjects = subjects;
-  }
-
-  @JsonProperty
-  public List<Subject> getSubjects() {
+  @JsonProperty public List<URI> getSubjects() {
     return subjects;
   }
 
-  @JsonProperty
-  public void setSubjects(List<Subject> subjects) {
+  @JsonProperty public void setSubjects(List<URI> subjects) {
     this.subjects = subjects;
   }
 }
