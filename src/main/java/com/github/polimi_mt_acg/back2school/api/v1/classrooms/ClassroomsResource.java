d2eab1721326e63a5541fd084ead92317f32bfe0
GG <giacomo.gregg@gmail.com>
2018-05-28 17:56:32 +0200

    Completed ClassroomResource

--- src/main/java/com/github/polimi_mt_acg/back2school/api/v1/classrooms/ClassroomsResource.java
+++ src/main/java/com/github/polimi_mt_acg/back2school/api/v1/classrooms/ClassroomsResource.java
@@ -45,33 +45,84 @@ public class ClassroomsResource {
   @POST
   @Consumes(javax.ws.rs.core.MediaType.APPLICATION_JSON)
   @AdministratorSecured
-  public Response postClassrooms(Classroom classroom, @Context HttpHeaders hh) {
+  public Response postClassrooms(Classroom classroom, @Context UriInfo uriInfo) {
 
-    DatabaseHandler dhi = DatabaseHandler.getInstance();
-    Session session = dhi.getNewSession();
+    // Check if a classroom with same name already exists, if so, do nothing
+    DatabaseHandler dbi = DatabaseHandler.getInstance();
+    Session session = dbi.getNewSession();
     session.beginTransaction();
+    List<Classroom> result =
+            dbi.getListSelectFromWhereEqual(Classroom.class, Classroom_.name, classroom.getName(), session);
+    if (!result.isEmpty()) {
+      session.getTransaction().commit();
+      session.close();
+      return Response.status(Response.Status.CONFLICT).entity("Classroom already exists.").build();
+    }
+
+    // Otherwise we accept the request.
     session.persist(classroom);
-    session.getTransaction().commit();
+    session.getTransaction().commit(); // Makes classrrom persisted.
     session.close();
 
-    return Response.ok(classroom, MediaType.APPLICATION_JSON).build();
+    // Now student has the ID field filled by the ORM
+    UriBuilder builder = uriInfo.getAbsolutePathBuilder();
+    URI uri = builder.path(String.valueOf(classroom.getId())).build();
+    return Response.created(uri).build();
   }
 
-  @Path("{id}")
+  @Path("{id: [0-9]+}")
   @GET
   @Consumes(javax.ws.rs.core.MediaType.APPLICATION_JSON)
   @AdministratorSecured
-  public void getClassroomID (@PathParam("id") int id){
-    DatabaseHandler dhi = DatabaseHandler.getInstance();
-    Session session = dhi.getNewSession();
-    session.beginTransaction();
-    List<Classroom> classrooms =
+  public Response getClassroomID (@PathParam("id") String classroomId){
+    // Fetch Classroom
+    List<Classroom> res =
             DatabaseHandler.getInstance()
-                    .getListSelectFromWhereEqual(Classroom.class, Classroom_.id, id, session);
+                    .getListSelectFromWhereEqual(Classroom.class, Classroom_.id, Integer.parseInt(classroomId));
 
-    session.close();
-//    return new ClassroomsResponse(classrooms);
+    if (res.isEmpty()) {
+      return Response.status(Response.Status.NOT_FOUND).entity("Unknown classroom id").build();
+    }
+
+    Classroom classroom = res.get(0);
+    return Response.ok(classroom, MediaType.APPLICATION_JSON_TYPE).build();
   }
 
 
+  @Path("{id: [0-9]+}")
+  @PUT
+  @Consumes(MediaType.APPLICATION_JSON)
+  @AdministratorSecured
+  public Response putClassroomById(PutClassroomRequest newClassroom, @PathParam("id") String classroomId) {
+    // Fetch Classroom
+    DatabaseHandler dbi = DatabaseHandler.getInstance();
+    Session session = dbi.getNewSession();
+    session.beginTransaction();
+
+    List<Classroom> res =
+            dbi.getListSelectFromWhereEqual(Classroom.class, Classroom_.id, Integer.parseInt(classroomId), session);
+
+    if (res.isEmpty()) {
+      session.getTransaction().commit();
+      session.close();
+      return Response.status(Response.Status.NOT_FOUND).entity("Unknown classroom id").build();
+    }
+
+    Classroom classroom = res.get(0);
+    updateClassroom(classroom, newClassroom);
+    session.getTransaction().commit();
+    session.close();
+
+    // According to HTTP specification:
+    // HTTP status code 200 OK for a successful PUT of an update to an existing resource. No
+    // response body needed.
+    return Response.ok().build();
+  }
+
+  private void updateClassroom(Classroom classroom, PutClassroomRequest newClassroom) {
+    classroom.setName(newClassroom.getName());
+    classroom.setFloor(newClassroom.getFloor());
+    classroom.setBuilding(newClassroom.getBuilding());
+
+  }
 }
