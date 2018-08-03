e821d76c79bb7dbcf910f0d3deb241d78f8f8804
nyxgear <dev@nyxgear.com>
2018-08-03 16:14:15 +0200

    Add test for notifications_read

--- src/test/java/com/github/polimi_mt_acg/back2school/utils/json_mappers/NotificationsReadJSONTemplate.java
+++ src/test/java/com/github/polimi_mt_acg/back2school/utils/json_mappers/NotificationsReadJSONTemplate.java
@@ -1,13 +1,24 @@
 package com.github.polimi_mt_acg.back2school.utils.json_mappers;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.util.List;
 
 public class NotificationsReadJSONTemplate implements JSONTemplateInterface {
 
+  @JsonProperty("notifications_read")
   public List<SeedEntityNotificationRead> notificationsRead;
 
   @Override
   public List<?> getEntities() {
     return notificationsRead;
+  }
+
+  public List<SeedEntityNotificationRead> getNotificationsRead() {
+    return notificationsRead;
+  }
+
+  public void setNotificationsRead(List<SeedEntityNotificationRead> notificationsRead) {
+    this.notificationsRead = notificationsRead;
   }
 }
