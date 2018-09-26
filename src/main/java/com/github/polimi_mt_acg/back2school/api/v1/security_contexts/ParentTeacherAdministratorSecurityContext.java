2904503a927e4f0f8f75fd932c3e271b4c4b08b1
nyxgear <dev@nyxgear.com>
2018-09-26 07:54:15 +0200

    ParentAdministrator, TeacherAdministrator, ParentTeacherAdministrator improved

--- src/main/java/com/github/polimi_mt_acg/back2school/api/v1/security_contexts/ParentTeacherAdministratorSecurityContext.java
+++ src/main/java/com/github/polimi_mt_acg/back2school/api/v1/security_contexts/ParentTeacherAdministratorSecurityContext.java
@@ -1,5 +1,6 @@
 package com.github.polimi_mt_acg.back2school.api.v1.security_contexts;
 
+import com.github.polimi_mt_acg.back2school.api.v1.StatusResponse;
 import com.github.polimi_mt_acg.back2school.model.AuthenticationSession;
 import com.github.polimi_mt_acg.back2school.model.User;
 import com.github.polimi_mt_acg.back2school.model.User.Role;
@@ -11,43 +12,48 @@ import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.Provider;
 
 /**
- * AdministratorSecurityContext implements a request filter for JAX-RS REST APIs. It implements a
- * "Administrators-only" security policy. A REST API that is annotated with @AdministratorSecured
- * can only be accessed if: a) the client is authenticated. b) the client role is ADMINISTRATOR
+ * ParentTeacherAdministratorSecurityContext implements a request filter for JAX-RS REST APIs.
+ *
+ * <p>It implements a "ParentsOrTeachersOrAdministrators-only" security policy.
+ *
+ * <p>A REST API annotated with this, can only be accessed if:
+ *
+ * <p>a) the client is authenticated.
+ *
+ * <p>b) the client role is PARENT or TEACHER or ADMINISTRATOR
+ *
+ * <p>To be successfully authenticated, the client must send the auth token in the AUTHORIZATION
+ * HTTP header.
  */
 @ParentTeacherAdministratorSecured
 @Provider
 @Priority(SecurityContextPriority.PARENT_TEACHER_ADMINISTRATOR)
 public class ParentTeacherAdministratorSecurityContext implements ContainerRequestFilter {
 
-  /**
-   * Filter requests that do not match the following security conditions: a) the client is
-   * authenticated. b) the client role is PARENT/TEACHER/ADMINISTRATOR.
-   *
-   * <p>To be successfully authenticated, the client must send the auth token in the AUTHORIZATION
-   * HTTP header.
-   *
-   * <p>If any of the above conditions are not met the request is dropped and the client is returned
-   * a UNAUTHORIZED Error Response.
-   */
   @Override
-  public void filter(ContainerRequestContext requestContext) throws IOException {
+  public void filter(ContainerRequestContext requestContext) {
     User currentUser = AuthenticationSession.getCurrentUser(requestContext);
 
     // Abort the filter chain with a 401 status code response
 
     // if there is no user logged in
     if (currentUser == null) {
-      requestContext.abortWith(Response.status(Response.Status.UNAUTHORIZED).build());
+      requestContext.abortWith(
+          Response.status(Response.Status.UNAUTHORIZED)
+              .entity(new StatusResponse(Response.Status.UNAUTHORIZED, "Not a valid session"))
+              .build());
       return;
     }
 
-    // if the user logged has not the correct role
-    Role role = currentUser.getRole();
-    if (!role.equals(Role.PARENT)
-        && !role.equals(Role.TEACHER)
-        && !role.equals(Role.ADMINISTRATOR)) {
-      requestContext.abortWith(Response.status(Response.Status.UNAUTHORIZED).build());
+    // if the user logged in has not the correct role
+    if (!currentUser.getRole().equals(Role.PARENT)
+        && !currentUser.getRole().equals(Role.TEACHER)
+        && !currentUser.getRole().equals(Role.ADMINISTRATOR)) {
+      requestContext.abortWith(
+          Response.status(Response.Status.UNAUTHORIZED)
+              .entity(new StatusResponse(Response.Status.UNAUTHORIZED, "User not allowed"))
+              .build());
+      return;
     }
   }
 }
