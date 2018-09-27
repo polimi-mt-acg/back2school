44c3d4ec44069e1593982a396963b4832e93da31
nyxgear <dev@nyxgear.com>
2018-09-27 17:03:26 +0200

    conformed TeacherAppointmentRequest.java to ValidableRequest

--- src/main/java/com/github/polimi_mt_acg/back2school/api/v1/teachers/TeacherAppointmentRequest.java
+++ src/main/java/com/github/polimi_mt_acg/back2school/api/v1/teachers/TeacherAppointmentRequest.java
@@ -1,11 +1,23 @@
 package com.github.polimi_mt_acg.back2school.api.v1.teachers;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.github.polimi_mt_acg.back2school.api.v1.StatusResponse;
+import com.github.polimi_mt_acg.back2school.api.v1.ValidableRequest;
 import com.github.polimi_mt_acg.back2school.model.Appointment;
+import com.github.polimi_mt_acg.back2school.model.Appointment_;
+import com.github.polimi_mt_acg.back2school.model.User;
+import com.github.polimi_mt_acg.back2school.model.User_;
+import com.github.polimi_mt_acg.back2school.utils.DatabaseHandler;
 
+import javax.ws.rs.core.Response;
 import java.time.LocalDateTime;
+import java.util.Optional;
 
-public class TeacherAppointmentRequest {
+public class TeacherAppointmentRequest implements ValidableRequest {
+
+  @JsonIgnore private Response invalidPostResponse;
+  @JsonIgnore private Response invalidPutResponse;
 
   @JsonProperty("parent_id")
   private Integer parentId;
@@ -48,5 +60,93 @@ public class TeacherAppointmentRequest {
 
   public void setStatus(Appointment.Status status) {
     this.status = status;
+  }
+
+  @Override
+  @JsonIgnore
+  public boolean isValidForPost() {
+    if (getParentId() == null || getParentId() == 0) {
+      invalidPostResponse =
+          Response.status(Response.Status.BAD_REQUEST)
+              .entity(
+                  new StatusResponse(
+                      Response.Status.BAD_REQUEST, "Missing required attribute: parent_id"))
+              .build();
+      return false;
+    }
+
+    Optional<User> userOpt = DatabaseHandler.fetchEntityBy(User.class, User_.id, getParentId());
+    if (!userOpt.isPresent() || !userOpt.get().getRole().equals(User.Role.PARENT)) {
+      invalidPostResponse =
+          Response.status(Response.Status.BAD_REQUEST)
+              .entity(new StatusResponse(Response.Status.BAD_REQUEST, "Unknown parent id"))
+              .build();
+      return false;
+    }
+
+    if (getDatetimeStart() == null || getDatetimeStart().toString().isEmpty()) {
+      invalidPostResponse =
+          Response.status(Response.Status.BAD_REQUEST)
+              .entity(
+                  new StatusResponse(
+                      Response.Status.BAD_REQUEST, "Missing required attribute: datetime_start"))
+              .build();
+      return false;
+    }
+
+    if (getDatetimeEnd() == null || getDatetimeEnd().toString().isEmpty()) {
+      invalidPostResponse =
+          Response.status(Response.Status.BAD_REQUEST)
+              .entity(
+                  new StatusResponse(
+                      Response.Status.BAD_REQUEST, "Missing required attribute: datetime_end"))
+              .build();
+      return false;
+    }
+
+    return true;
+  }
+
+  @Override
+  @JsonIgnore
+  public Response getInvalidPostResponse() {
+    return invalidPostResponse;
+  }
+
+  @Override
+  @JsonIgnore
+  public boolean isValidForPut(Integer id) {
+    Optional<Appointment> appointmentOpt =
+        DatabaseHandler.fetchEntityBy(Appointment.class, Appointment_.id, id);
+    if (!appointmentOpt.isPresent()) {
+      invalidPutResponse =
+          Response.status(Response.Status.NOT_FOUND)
+              .entity(new StatusResponse(Response.Status.NOT_FOUND, "Unknown appointment id"))
+              .build();
+      return false;
+    }
+
+    if (!isValidForPost()) {
+      invalidPutResponse = invalidPostResponse;
+      return false;
+    }
+
+    if (getStatus() == null || getStatus().toString().isEmpty()) {
+      invalidPutResponse =
+          Response.status(Response.Status.BAD_REQUEST)
+              .entity(
+                  new StatusResponse(
+                      Response.Status.BAD_REQUEST, "Missing required attribute: status"))
+              .build();
+      return false;
+    }
+
+    return true;
+  }
+
+  @Override
+  @JsonIgnore
+  public Response getInvalidPutResponse() {
+    return invalidPutResponse;
   }
 }
